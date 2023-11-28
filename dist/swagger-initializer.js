54fa8142fd26af726def5f0884952c50b76126c1
Miguel Pinilla <jmpicnic@gmail.com>
2023-11-28 13:20:30 -0800

    Enable url query for index.

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,15 @@
+const queryArray = window.location.search.split('=')
+const specPath = queryArray.length > 1 ? queryArray[1] : "swagger.yaml";
+console.log("FILEPATH: "+filepath);
+
+
+
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: specPath, // "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
