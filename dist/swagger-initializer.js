408dee713a827c6f79ff3c9d41b6a64022f50a5c
Miguel Pinilla <jmpicnic@gmail.com>
2023-11-28 13:23:32 -0800

    in function

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,11 +1,8 @@
-const queryArray = window.location.search.split('=')
-const specPath = queryArray.length > 1 ? queryArray[1] : "swagger.yaml";
-console.log("FILEPATH: "+filepath);
-
-
-
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+  let queryArray = window.location.search.split('=')
+  let specPath = queryArray.length > 1 ? queryArray[1] : "swagger.yaml";
+  console.log("FILEPATH: "+filepath);
+    //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
